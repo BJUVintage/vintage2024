1d71fe9ac02c9008ffe0c445fb8e3b3529178a2b
Rhys Fuller <rfull917@students.bju.edu>
2024-04-25 16:21:39 -0400

    minor index fix

--- components/GroupsMini.vue
+++ components/GroupsMini.vue
@@ -3,7 +3,7 @@
 <template>
   <page-title :title="'Groups'" svg="Sun.svg" class="groups" svgClass="groupsSVG"/>
   <basic-layout
-    big-photo-name="vball.jpg"
+    big-photo-name="/groups/academic.jpg"
     title="Academic Teams"
     description="Academic teams in robotics, biology and computer programming develop innovative skills and provide valuable competition and career-focused activities outside the classroom. These programs provide students the unique opportunity to work with peers in their specific areas of interest. The team members’ collaboration on year-long projects prepares students to participate in state and nation-wide events. By stimulating teams intellectually and relationally, these challenges develop competency in academics and leadership. "
     button-page="groups"
