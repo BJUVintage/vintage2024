6a1d3ac1294a27618896726349a547682eebce99
Rhys Fuller <rfull917@students.bju.edu>
2024-04-25 20:13:12 -0400

    more formatting

--- components/EventsMini.vue
+++ components/EventsMini.vue
@@ -2,15 +2,9 @@
 
 <template>
   <page-title title="Events" svg="Bird.svg" class="events" svgClass="eventSVG" />
-  <basic-layout
-    big-photo-name="/events/welcome.jpg"
-    title="Welcome Week"
+  <basic-layout big-photo-name="/events/welcome.jpg" title="Welcome Week"
     description="Welcome Week introduced the Class of 2027, BJU’s 100th graduating class, to campus life. With wide eyes and smiles, many in the freshman class began the college experience by moving into the residence halls. The Welcome Week theme—OWN IT—encouraged students to accept personal responsibility for their studies, personal relationships, and spiritual growth. The week sponsored “meet and greet” sessions for academic majors, orientation for new students, and special sessions for international students. Opening exercises and evangelistic meetings established the foundation for a successful year. "
-    button-page="events"
-    button-text="All Events"
-    big-photo-right
-    is-mini
-  ></basic-layout>
+    button-page="events" button-text="All Events" big-photo-right is-mini></basic-layout>
 </template>
 
 <script>
