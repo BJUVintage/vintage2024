c95e391e39669e09ad1c73f4176df75ad62d0f46
Rhys Fuller <rfull917@students.bju.edu>
2024-04-25 20:12:56 -0400

    formatting

--- components/GroupsMini.vue
+++ components/GroupsMini.vue
@@ -1,21 +1,16 @@
 <script lang="ts" setup></script>
 
 <template>
-  <page-title :title="'Groups'" svg="Sun.svg" class="groups" svgClass="groupsSVG"/>
-  <basic-layout
-    big-photo-name="/groups/academic.jpg"
-    title="Academic Teams"
+  <page-title :title="'Groups'" svg="Sun.svg" class="groups" svgClass="groupsSVG" />
+  <basic-layout big-photo-name="/groups/academic.jpg" title="Academic Teams"
     description="Academic teams in robotics, biology and computer programming develop innovative skills and provide valuable competition and career-focused activities outside the classroom. These programs provide students the unique opportunity to work with peers in their specific areas of interest. The team members’ collaboration on year-long projects prepares students to participate in state and nation-wide events. By stimulating teams intellectually and relationally, these challenges develop competency in academics and leadership. "
-    button-page="groups"
-    button-text="All Groups"
-    is-mini
-  ></basic-layout>
+    button-page="groups" button-text="All Groups" is-mini></basic-layout>
 </template>
 
 <script>
 export default {
   name: "GroupsMini",
-  }
+}
 </script>
 
 <style scoped></style>
