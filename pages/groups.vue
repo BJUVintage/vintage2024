31ad1162e6884ea7eb4ccef6f6a780fab44b41cd
Rhys Fuller <rfull917@students.bju.edu>
2024-04-25 21:16:09 -0400

    added sports/choir in groups

--- pages/groups.vue
+++ pages/groups.vue
@@ -13,13 +13,42 @@
   <basic-layout big-photo-name="/groups/aquaponic.jpg" title="Research Teams"
     description="STEM courses offer opportunities for students to apply classroom skills in real-world scenarios. Biology, chemistry, and engineering majors, partner with companies to engage in research. These teams sharpen critical thinking, troubleshooting, and data-analysis skills, as well as promote teamwork. This year, the BJU Engineering Department conducted a research project in BJU’s greenhouse. Students and faculty developed an aquaponics system, an arrangement in which twenty-five tilapia feed on plants and thereby allow those plants to flourish. The research project was part of this year’s Global Challenges class project, pursuing “Honduras Sin Hambre” (Honduras Without Hunger). In addition to STEM competitions, biology majors conduct research in BJU’s cancer lab each summer. Lab members not only expand knowledge about cancer but also experience the practical routine of a research career. Students learn to propose and conduct experiments, interpret their results, and communicate their findings in a professional presentation."></basic-layout>
 
+  <basic-layout big-photo-name="/groups/vball.jpg" title="Volleyball"
+    description="After a challenging season, the Bruins volleyball team secured the NCCAA DII National Championship, defeating Manhattan Christian College 3-0. The season ended on a nine-game win streak with a 35-5 season record. This success marked the team’s sixth successive season of winning the national title. Kiersten Hoopes and Victoria Glaze earned 1st team All-American honors, and Victoria Glaze was named the Division II National Player of the Year. Coach Vickie Denny surpassed 1,200 career victories. She became the sixth coach in collegiate women's volleyball history to reach this milestone. "
+    big-photo-right></basic-layout>
+
+  <basic-layout big-photo-name="/groups/golf.jpg" title="Golf"
+    description="The Bruins Golf Team enjoyed a record-breaking season. The team posted a score of 291 in Round 1 of the NCCAA South Region Championship, and Nicholas Smith achieved the school record of 64 in Round 2 of the Alice Lloyd Fall Invitational. In October, the team received an at-large bid to participate in the National Christian College Athletic Association. After three difficult rounds on Hammock Beach in Palm Coast, Florida, the Bruins finished in 9th place. In individual standings Timmy Smith finished in 22nd place and Brandon Autry 24th."></basic-layout>
+
+  <basic-layout big-photo-name="/groups/track.jpg" title="Cross Country and Track and Field"
+    description="The women’s cross country team captured its fourth straight NCCAA National Championship. Following the race, team members Lauren Jones, Joanna Lain, and Liesl Heinz earned All-American honors. The men’s cross country team secured a victory at the NCCAA National Championship, a title the team has held for eight years. Levi Gerbitz, Trenton Goldsmith, Joseph Grassmid, Marc Johnson, and Sol Shafer earned All-American honors. Head Coach Ken Roach was named the NCCAA DII Coach of the Year for the third time. The Bruins Track and Field team competed in an indoor competition this season. Joseph Grassmid finished third in the 3000 meter steeplechase, and Brian Sims finished first in the 400 meter race. At the conclusion of competition, the men's team earned eighth place out of 21 teams and the women's team thirteenth place."
+    big-photo-right></basic-layout>
+
+  <basic-layout big-photo-name="/groups/soccer.jpg" title="Soccer"
+    description="The Bruins men's soccer team fell in a 2-0 decision to Pensacola Christian College in the NCCAA DII South Region Championship match. The Bruins ended the 2023 season with an 11-5-3 season record and were 2-0-1 in conference play. Team members Mario Figueroa and James McCarty earned All-American honors. The Bruins women’s team remained undefeated for 13 games and secured a victory at the NCCAA DI South Region Tournament, defeating Campbellsville University Harrodsburg 7-0. Mia Marks, Margaret Cummings, Braden Adams, and Gabriela Gonzalez earned All-American honors, and Gabriela Gonzalez was named the National Player of the Year. The team went on to capture the national championship in double overtime. Head Coach Chris Carmichael received the National Coach of the Year award."></basic-layout>
+
+  <basic-layout big-photo-name="/groups/bball.jpg" title="Basketball"
+    description="Last season, the Bruins men’s basketball team concluded the NCCAA South Region Tournament with a 91-71 loss against the Toccoa Falls College Screaming Eagles. The Bruins season ended with a 12-13 record. The team began the 2023-24 season with eight consecutive undefeated games in the NCCAA South Region, competed in the NCCAA South Region Tournament in Pensacola, Florida. The women’s basketball team’s last season ended with an 11-14 record after finishing third in the NCCAA South Region Tournament. This year, the team remained undefeated for five games and competed in the NCCAA South Region Tournament."
+    big-photo-right></basic-layout>
+
+  <basic-layout big-photo-name="/groups/baseball.jpg" title="Baseball"
+    description="Last year, the Bruins baseball team’s season ended in the NCCAA South Region Tournament with a 3-1 loss to Trinity Baptist College. At the conclusion of the tournament, team member Conley Burk was named to the NCCAA South All-Region 2nd team for the 2023 season. This year, the team opened its fourth season of competition, the first game in a busy 37-game schedule. The highlight of the season was the Bruins’ hosting eleven of the twelve games at the back end of their season. The team sought to improve its 2023 campaign and welcome another strong recruiting class."></basic-layout>
+
+  <basic-layout big-photo-name="/groups/instr.jpg" title="Instrumental Groups"
+    description="Bob Jones University’s instrumental groups cultivate students’ musical talent. These groups include the University Symphony Orchestra and Symphonic Wind Band and choices for handbell, harp, trumpet, tuba-euphonium, trombone, flute, and saxophone as well as the horn ensemble. Students rehearse many hours in preparation for their concerts, which often feature hymns and classical pieces. In October, ensembles participated in the High School Festival “Kaleidoscope Concert.” Other musical groups performed ministry outreaches at local churches."
+    big-photo-right></basic-layout>
+
+  <basic-layout big-photo-name="/groups/choir.jpg" title="Choral Groups"
+    description="Five choral groups offer students on every level of musicianship an opportunity to showcase talent for God’s glory. This year, the 100-voice University Singers initiated the concert season with “God’s Great Mysteries.” In November, the Concert Choir presented “All of Our Tomorrows,” featuring Requiem, a work by Heather Sorenson and commissioned in honor of those who lost their lives during the Covid-19 pandemic. Then, the Lyric Choir celebrated Christmas with their program, “Sing Noel,” a selection of holiday favorites. Finally, the University Chorale performed “From Darkness to Light.” In addition to concerts, choral group minister in local churches throughout the year to encourage congregations and praise the Creator. "></basic-layout>
+
+
   <basic-layout big-photo-name="/groups/slc.jpg" title="Student Leadership Council"
     description="The Student Leadership Council (SLC), eleven officers nominated by their peers, plans and promotes student body events. SLC members also serve as liaisons between students and the administration. Two main events hosted by the SLC, Spirit Week and Ignite, create unity through recreational activities and praise services. In addition to sponsoring events, the SLC chooses the academic year’s spiritual theme. The 2023-24 theme was “By My Spirit,” a study of Zechariah 4:6."
     little-photo-name="/groups/turkey-bowl-2.jpeg" big-photo-right></basic-layout>
 
-
   <basic-layout big-photo-name="/groups/isc.jpg" title="Inter-Society Council"
     description="The Inter-Society Council (ISC) is comprised of student leaders on campus who serve as society presidents, vice-presidents, and chaplains. These students have the opportunity to develop as leaders by influencing their peers and impacting their community for God's glory. The Inter-Society Executive Committee (ISCEC) oversees and organizes ISC events and personally invests in ISC members to equip them for increased leadership effectiveness. The ISCEC includes the ISC directors, assistant directors, and chaplains, as well as the athletic directors, secretary, treasurer, and chorister."></basic-layout>
+
 
 </template>
 
