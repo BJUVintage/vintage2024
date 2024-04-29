181b497ab5f9976ac9973922abe18f3b123e014a
Elias Witt <wittbrass@icloud.com>
2024-04-29 18:13:09 -0400

    fix: typos, modify nuxt config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content'
-  ],
+  target: 'static',
   app: {
     head: {
       title: 'Vintage 2024',
